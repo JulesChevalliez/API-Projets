ea2e1883b6129a7a8403070d6d44013a660c690d
JulesChevalliez <56346843+JulesChevalliez@users.noreply.github.com>
2019-12-17 14:45:32 +0100

    Add files via upload

--- client_comment/public/CommentClient.js
+++ client_comment/public/CommentClient.js
@@ -1,142 +1,127 @@
 const Url = "http://localhost/MicroServiceComment/api_comment/public/index.php";
 let uuid = 0;
 
-$(document).ready(function () {
-    $('#sendForm').on('click', function () {
-        arrayForm = $("#formComment").serializeArray();
-        const FullUrl = Url + "/api/addComment";
-        if (localStorage.getItem("uuid") === null) {
-            alert("Désolé une erreur est survenue !")
-        } else if (arrayForm[0].value === "") {
-            alert("Vous n'avez pas renseigner de nom")
-        } else if (arrayForm[1].value === "") {
-            alert("Votre message est vide");
-        } else {
-            uuid = localStorage.getItem("uuid");
-            $.ajax({
-                url: FullUrl,
-                type: "GET",
-                data: {uuid: uuid, name: arrayForm[0].value, comment: arrayForm[1].value},
-                success: function (result) {
-                    comments();
-                    $('#name').val("");
-                    $('#msg').html("");
-                },
-                error: function (error) {
-                    console.log(error);
-                }
-            });
-        }
-    });
-
-    $(document).on('click', '.comment-edit', function () {
-        $('#editForm').show();
-        $('#commentForm').hide();
-        let element = $(this).attr('data-edit-id');
-        let trElement = $(this)[0].parentElement.parentElement.firstChild;
-        console.log($(this)[0].parentElement.parentElement.nextSibling.firstChild);
-        let nom = trElement.innerHTML;
-        let comment = $(this)[0].parentElement.parentElement.nextSibling.firstChild.innerHTML;
-        localStorage.setItem("idComment", element);
-        $('#name').val(nom);
-        $('#msg').html(comment);
-        window.location = "#editForm";
-    });
-
-    $('#sendEdit').on('click', function () {
-        arrayForm = $("#forEdit").serializeArray();
-        if (localStorage.getItem("idComment") === null) {
-            alert("Une erreur est survenue !")
-        } else if (arrayForm[1].value === "") {
-            alert("Votre message est vide");
-
-        } else {
-            let idToEdit = localStorage.getItem("idComment");
-            $('#editForm').hide();
-            $('#commentForm').show();
-            $('#name').val("");
-            $('#msg').html("");
-            const FullUrl = Url + "/api/editComment";
-            $.ajax({
-                url: FullUrl,
-                type: "POST",
-                data: {id: idToEdit, comment: arrayForm[1].value},
-                success: function (result) {
-                    comments();
-                },
-                error: function (error) {
-                    console.log(error);
-                }
-            });
-        }
-
-    });
-
-    $('.btnLireTicket').on('click', function () {
-        uuid = $(this).attr('data-id-ticket');
-        localStorage.setItem("uuid", uuid);
-        console.log(localStorage.getItem("uuid"));
-    });
-
-    function comments() {
-        const FullUrl = Url + "/api/comment";
-        if (localStorage.getItem("uuid") === null) {
-            alert("Désolé une erreur est survenue !")
-        } else {
-            uuid = localStorage.getItem("uuid");
-            $.ajax({
-                url: FullUrl,
-                type: "GET",
-                data: {uuid: uuid},
-                success: function (result) {
-                    console.log(result);
-                    let template = '';
-                    for (let i = 0; i < result.length; i++) {
-                        template += '<div class="card cardComment">' +
-                                        '<div class="card-header">' +
-                                            '<span>' + result[i]['username']+'</span>'+
-                                            '<ul class="nav nav-tabs card-header-tabs float-right">' +
-                                                '<button class="btn btn-primary fa fa-edit btnCard comment-edit" data-edit-id="'+result[i]['id']+'"></button>' +
-                                                '<button class="btn btn-warning fa fa-trash btnCard comment-delete" data-id="'+result[i]['id']+'"></button>' +
-                                            '</ul>' +
-                                        '</div>' +
-                                        '<div class="card-body">' +
-                                            '<p class="card-text">'+result[i]['comment']+'</p>' +
-                                            '<p class="card-text float-right"><small class="text-muted">'+result[i]['date']+'</small></p>' +
-                                        '</div>' +
-                                    '</div>';
-                    }
-                    $('.blockComment').html(template);
-                },
-                error: function (error) {
-                    console.log(error);
-                }
-            });
-        }
-
+if (window.template){
+    if (template){
+        tplt = template;
+    }else{
+        tplt = "";
     }
+}else {
+    tplt = "";
+}
 
-    comments();
 
-
-    $(document).on('click', '.comment-delete', function () {
-        let element = $(this).attr('data-id');
-        const FullUrl = Url + "/api/deleteComment";
-        console.log(FullUrl);
+function addCom(uuid, username, comment) {
+    const FullUrl = Url + "/api/addComment";
+    if (uuid === null) {
+        alert("Désolé une erreur est survenue !")
+    } else if (username === "") {
+        alert("Vous n'avez pas renseigner de nom")
+    } else if (comment === "") {
+        alert("Votre message est vide");
+    } else {
         $.ajax({
             url: FullUrl,
-            type: "POST",
-            data: {id: element},
+            type: "GET",
+            data: {uuid: uuid, name: username, comment: comment},
             success: function (result) {
-                comments();
-                console.log(result);
+                comments(uuid);
             },
             error: function (error) {
                 console.log(error);
             }
         });
+    }
+}
 
+function editCom(id, comment, uuid) {
+    const FullUrl = Url + "/api/editComment";
+
+    if (localStorage.getItem("idComment") === null) {
+        alert("Une erreur est survenue !")
+    } else if (comment === "") {
+        alert("Votre message est vide");
+    } else if (id === null) {
+        alert("Une erreur est survenue !")
+    } else {
+        $.ajax({
+            url: FullUrl,
+            type: "POST",
+            data: {id: id, comment: comment},
+            success: function (result) {
+                comments(uuid);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+}
+
+function deleteCom(id, uuid) {
+    const FullUrl = Url + "/api/deleteComment";
+    $.ajax({
+        url: FullUrl,
+        type: "POST",
+        data: {id: id},
+        success: function (result) {
+            comments(uuid);
+            console.log(result);
+        },
+        error: function (error) {
+            console.log(error);
+        }
     });
+}
 
-});
+function comments(uuid) {
+    const FullUrl = Url + "/api/comment";
 
+
+    if (uuid === 0) {
+        alert("Désolé une erreur est survenue !")
+    } else {
+        $.ajax({
+            url: FullUrl,
+            type: "GET",
+            data: {uuid: uuid},
+            success: function (result) {
+                console.log(result);
+                let template = '';
+                if (!tplt) {
+                    for (let i = 0; i < result.length; i++) {
+                        template += '<div class="card cardComment">' +
+                            '<div class="card-header">' +
+                            '<span id="user_' + result[i]['id'] + '">' + result[i]['username'] + '</span>' +
+                            '<ul class="nav nav-tabs card-header-tabs float-right">' +
+                            '<button class="btn btn-primary fa fa-edit btnCard comment-edit" data-edit-id="' + result[i]['id'] + '"></button>' +
+                            '<button class="btn btn-warning fa fa-trash btnCard comment-delete" data-id="' + result[i]['id'] + '"></button>' +
+                            '</ul>' +
+                            '</div>' +
+                            '<div class="card-body">' +
+                            '<p class="card-text" id="comment_' + result[i]['id'] + '">' + result[i]['comment'] + '</p>' +
+                            '<p class="card-text float-right"><small class="text-muted">' + result[i]['date'] + '</small></p>' +
+                            '</div>' +
+                            '</div>';
+                    }
+                } else {
+
+                    for (let i = 0; i < result.length; i++) {
+                        temp = tplt;
+                        temp = temp.replace("!username!", result[i]['username']);
+                        temp = temp.replace(/!id!/g, result[i]['id']);
+                        temp = temp.replace("!comment!", result[i]['comment']);
+                        temp = temp.replace("!date!", result[i]['date']);
+                        template += temp;
+                    }
+                }
+                $('.blockComment').html(template);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+
+}
